7a2bda13f4ee5965087bd507b049ee52b08b3bfe
spystation <charles@spystation.com>
2015-02-22 22:19:01 -0500

    Update per Planet Pizza Assignment

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -11,6 +11,47 @@ app.controller('MainController', ['$scope', function($scope) {
       name: 'Mozzarella Sticks',
       description: 'Served with marinara sauce.',
       price: 3.95
+    },
+    {
+      name: 'Bruschetta',
+      description: 'Grilled bread garlic, tomatoes, olive oil.',
+      price: 4.95
+    }
+  ];
+
+  $scope.mains = [
+    {
+      name: 'Rigattoni ala Vodka',
+      description: 'Mozzarella, tomatoes, basil, balsmaic glaze.',
+      price: 8.95
+    },
+    {
+      name: 'Spaggetti with Meat Sauce',
+      description: 'Served with marinara meat sauce.',
+      price: 9.95
+    },
+    {
+      name: 'Fettucinni Alfredo',
+      description: 'Alfredo Sauce, Parmessan Cheese, olive oil.',
+      price: 7.95
+    }
+  ];
+
+  $scope.extras = [
+    {
+      name: 'Bread Sticks',
+      description: 'Served with marinara meat sauce.',
+      price: 4.95
+    },
+    {
+      name: 'Soup of the Day',
+      description: 'Take a chance.',
+      price: 4.95
+    },
+    {
+      name: 'Buffalo Wings',
+      description: 'Feel the power of wings.',
+      price: 6.95
     }
   ];
 
